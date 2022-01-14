dc7e999a170d91c8cdf2a0fed435e7f786b75e4e
Juan Mateo <throwaway4classjmateo@gmail.com>
2022-01-14 15:40:27 -0500

    add: model associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,40 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Save = require('./models/Save')
+const Stage = require('./models/Stage')
+const Dinosaur = require('./models/Dinosaur')
+const Item  = require('./models/Item')
+const Inventory  = require('./models/Inventory')
 
 //associations could go here!
+
+User.hasMany(Save)
+Save.belongsTo(User)
+
+Save.hasOne(Inventory)
+Inventory.belongsTo(Save)
+
+Save.belongsToMany(Stage)
+Stage.belongsToMany(Save)
+
+Stage.belongsToMany(Dinosaur)
+Dinosaur.belongsToMany(Stage)
+
+Save.belongsToMany(Item, { through: Inventory })
+Item.belongsToMany(Save, { through: Inventory })
+
+Dinosaur.belongsToMany(Item)
+Item.belongsToMany(Dinosaur)
 
 module.exports = {
   db,
   models: {
     User,
+    Save,
+    Stage,
+    Dinosaur,
+    Item,
+    Inventory
   },
 }
