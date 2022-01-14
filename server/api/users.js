de80745fa4380eb6a2fa482be8ab8144c112579f
Joel Bascombe <joelbascombe13@gmail.com>
2022-01-14 16:31:00 -0500

    edit: userSaveDate route added

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,38 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ["id", "username"],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const userSaveDate = await Order.findOne({
+      where: {
+        attributes: ["id", "username"],
+      },
+      include: [
+        {
+          model: Save,
+        },
+      ],
+    }); //Test to see if user exist send back 404 error
+    console.log("userSaveDate", userSaveDate);
+    res.send(userSaveDate);
+  } catch (error) {
+    next(error);
+  }
+});
