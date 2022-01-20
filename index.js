d809b3fbb072ad37ccd0caff42f6d75bc7a22f5b
am-hernandez <angel@mouseandweb.com>
2022-01-20 16:21:13 -0500

    chore: add player and physics

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: [PlayScene],
 }
 
